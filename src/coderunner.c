9e01771051a814716d5b2498ea160f9a7a4d4c8f
Yinyin Liao <yinyinl@gmail.com>
2013-07-29 01:37:11 +0800

    having real path before actual change workding directory and open log files

--- src/coderunner.c
+++ src/coderunner.c
@@ -17,9 +17,23 @@
 }
 
 
-static int prepare_log_files(const char *datafilename_stdin, const char *logfilename_stdout, const char *logfilename_stderr)
+
+static int check_working_directory(const char *working_directory, char **p_fullpath_working_directory)
+{
+	char *p;
+	if(NULL == (p = readlink(working_directory, NULL)))
+	{ return 1; }
+
+	*p_fullpath_working_directory = p;
+
+	return 0;
+}
+
+
+static int prepare_log_files(const char *datafilename_stdin, const char *logfilename_stdout, const char *logfilename_stderr, char **p_fullpath_datafile_stdin, char **p_fullpath_logfile_stdout, char **p_fullpath_logfile_stderr)
 {
 	int fd;
+	char *p;
 
 	if(NULL != datafilename_stdin)
 	{
@@ -33,7 +47,15 @@ static int prepare_log_files(const char *datafilename_stdin, const char *logfile
 			RECORD_ERR("failed on attempting to close STDIN file");
 			return 2;
 		}
+		if(NULL == (p = realpath(datafilename_stdin, NULL)))
+		{
+			RECORD_ERR("failed on getting path of STDIN file");
+			return 3;
+		}
+		*p_fullpath_datafile_stdin = p;
 	}
+	else
+	{ *p_fullpath_datafile_stdin = NULL; }
 
 	if(NULL != logfilename_stdout)
 	{
@@ -47,37 +69,70 @@ static int prepare_log_files(const char *datafilename_stdin, const char *logfile
 			RECORD_ERR("failed on attempting to close STDOUT file");
 			return 12;
 		}
+		if(NULL == (p = realpath(logfilename_stdout, NULL)))
+		{
+			RECORD_ERR("failed on getting path of STDOUT file");
+			return 13;
+		}
+		*p_fullpath_logfile_stdout = p;
 	}
+	else
+	{ *p_fullpath_logfile_stdout = NULL; }
 
-	if( (NULL != logfilename_stderr) && ( (NULL == logfilename_stdout) || (0 != strcmp(logfilename_stdout, logfilename_stderr)) ) )
+	if(NULL != logfilename_stderr)
 	{
-		if(-1 == (fd = open(logfilename_stderr, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP)))
+		if( (NULL == logfilename_stdout) || (0 != strcmp(logfilename_stdout, logfilename_stderr)) )
 		{
-			RECORD_ERR("failed on attempting to open STDERR file");
-			return 21;
+			if(-1 == (fd = open(logfilename_stderr, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP)))
+			{
+				RECORD_ERR("failed on attempting to open STDERR file");
+				return 21;
+			}
+			if(-1 == close(fd))
+			{
+				RECORD_ERR("failed on attempting to close STDERR file");
+				return 22;
+			}
 		}
-		if(-1 == close(fd))
+		if(NULL == (p = realpath(logfilename_stderr, NULL)))
 		{
-			RECORD_ERR("failed on attempting to close STDERR file");
-			return 22;
+			RECORD_ERR("failed on getting path of STDERR file");
+			return 23;
 		}
+		*p_fullpath_logfile_stderr = p;
 	}
+	else
+	{ *p_fullpath_logfile_stderr = NULL; }
 
 	return 0;
 }
 
 int run_program(CodeRunInstance *instance, const char *filename, char *const argv[], char *const envp[], const char *working_directory, const char *run_as_user, const char *datafilename_stdin, const char *logfilename_stdout, const char *logfilename_stderr, uint32_t max_running_second,  uint32_t overtime_sigint_second, uint32_t overtime_sigterm_second, uint32_t error_skip)
 {
+	char *fullpath_working_directory;
+	char *fullpath_datafile_stdin;
+	char *fullpath_logfile_stdout;
+	char *fullpath_logfile_stderr;
+
 	memset(instance, 0, sizeof(CodeRunInstance));
 
-	if(0 != working_directory(working_directory))
+	if(0 != check_working_directory(working_directory, &fullpath_working_directory))
+	{
+		RECORD_ERR("cannot have real path of given working directory");
+		return 1;
+	}
+
+	if(0 != prepare_log_files(datafilename_stdin, logfilename_stdout, logfilename_stderr, &fullpath_datafile_stdin, &fullpath_logfile_stdout, &fullpath_logfile_stderr))
+	{ return 2; }
+
+
+
+	if(0 != chdir(working_directory))
 	{
 		RECORD_ERR("failed on changing work directory");
 		return 1;
 	}
 
-	if(0 != prepare_log_files(datafilename_stdin, logfilename_stdout, logfilename_stderr))
-	{ return 2; }
 
 	return -1;
 }
