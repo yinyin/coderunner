7dabe8afe88560918f81421a275e96666965ea1b
Yinyin L <yinyinl@gmail.com>
2013-08-05 01:20:56 +0800

    add prototype of wait_program()

--- include/coderunner.h
+++ include/coderunner.h
@@ -29,6 +29,8 @@ typedef struct _T_CodeRunInstance {
 
 int run_program(CodeRunInstance *instance, const char *filename, char *const argv[], char *const envp[], const char *working_directory, const char *run_as_user, const char *datafilename_stdin, const char *logfilename_stdout, const char *logfilename_stderr, uint32_t max_running_second,  uint32_t overtime_sigint_second, uint32_t overtime_sigterm_second);
 
+int wait_program(CodeRunInstance *instance, int blocking_wait);
+
 
 
 #endif	/* __X_CODERUNNER_CODERUNNER_H__ */
