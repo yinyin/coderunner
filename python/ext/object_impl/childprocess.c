31952b28283972152368b9c198d0649c713085b1
Yinyin L <yinyinl@gmail.com>
2013-08-09 01:12:07 +0800

    add python extension

---
+++ python/ext/object_impl/childprocess.c
@@ -0,0 +1,364 @@
+/** implementation of methods belong to ChildProcess
+ *
+ * this code is intend to build with pycoderunner.c, should not build alone.
+ * */
+#ifndef ____CODE_INDICATOR_05a04832113c7891f7e5dffd4ef5a9a396a25058
+	#error "This code is not intent to build alone.\n"
+#endif
+
+#define MAX_ARGUMENT_BUFFER_SIZE 8192
+#define MAX_ARGUMENT_COUNT (511+1)
+#define MAX_ENVIRONMENT_BUFFER_SIZE 8192
+#define MAX_ENVIRONMENT_COUNT (511+1)
+
+#define CHILDPROCESS_BUFFER_ALLIGN 4
+#define CHILDPROCESS_BUFFER_ALLIGN_MASK (CHILDPROCESS_BUFFER_ALLIGN - 1)
+
+
+/* {{{ forward declares */
+static PyObject * ChildProcess_new(PyTypeObject *type, PyObject *args, PyObject *kwds);
+static void ChildProcess_dealloc(coderunner_ChildProcessObject *self);
+static int ChildProcess_init(coderunner_ChildProcessObject *self, PyObject *args, PyObject *kwds);
+
+static PyObject * ChildProcess_wait_blocking(coderunner_ChildProcessObject *self);
+static PyObject * ChildProcess_wait_nonblocking(coderunner_ChildProcessObject *self);
+/* }}} forward declares */
+
+
+static int _ChildProcess_prepare_arg(char *arg_buffer, char *arg_v[], PyObject *arg_obj)
+{
+	PyObject *iterator;
+	PyObject *item;
+	char *v_current_ptr;
+	int v_count;
+	int v_remain;
+
+	if( (NULL == arg_obj) || (!PyIter_Check(arg_obj)) )
+	{
+		_report_error(PyExc_TypeError, "ERR: given \"args\" parameter not support iterator interface. @[%s:%d]", __FILE__, __LINE__);
+		return -1;
+	}
+
+	if(NULL == (iterator = PyObject_GetIter(arg_obj)))
+	{
+		_report_error(PyExc_TypeError, "ERR: failed on getting iterator from \"args\". @[%s:%d]", __FILE__, __LINE__);
+		return -2;
+	}
+
+	v_current_ptr = arg_buffer;
+	v_count = 0;
+	v_remain = MAX_ARGUMENT_BUFFER_SIZE;
+	while( (v_remain > 0) && (v_count < (MAX_ARGUMENT_COUNT-1)) && (item = PyIter_Next(iterator)) ) {
+		char *p;
+		if(NULL == (p = PyString_AsString(item)))
+		{
+			_report_error(NULL, "ERR: cannot get string content from %d-th item of \"args\". @[%s:%d]", v_count, __FILE__, __LINE__);
+			Py_DECREF(item);
+			Py_DECREF(iterator);
+			return -3;
+		}
+
+		{
+			int l_arg;
+			int l_total;
+
+			l_arg = strlen(p);
+			l_total = l_arg + 1;
+
+			if(v_remain < l_total)
+			{
+				_report_error(PyExc_MemoryError, "ERR: insufficient memory for loading \"args\". @[%s:%d]", __FILE__, __LINE__);
+				Py_DECREF(item);
+				Py_DECREF(iterator);
+				return -4;
+			}
+
+			strncpy(v_current_ptr, p, l_total);
+			arg_v[v_count] = v_current_ptr;
+
+			l_total = (0 != (l_total & CHILDPROCESS_BUFFER_ALLIGN_MASK)) ? (
+					l_total + (CHILDPROCESS_BUFFER_ALLIGN - (l_total & CHILDPROCESS_BUFFER_ALLIGN_MASK))
+				) : l_total;
+
+			v_current_ptr += l_total;
+			v_remain -= l_total;
+		}
+
+		v_count++;
+		Py_DECREF(item);
+	}
+
+	arg_v[v_count] = NULL;
+
+	Py_DECREF(iterator);
+
+	return 0;
+}
+
+static int _ChildProcess_prepare_env(char *env_buffer, char *env_p[], PyObject *env_obj)
+{
+	PyObject *mapped_items;
+	PyObject *iterator;
+	PyObject *item;
+	char *v_current_ptr;
+	int v_count;
+	int v_remain;
+
+	if( (NULL == env_obj) || (!PyMapping_Check(env_obj)) )
+	{
+		strncpy(env_buffer, "EMPTY_ENV=1", 16);
+		env_p[0] = env_buffer;
+		env_p[1] = NULL;
+		#if DUMP_DEBUG_MSG
+			fprintf(stderr, "INFO: with empty environment variables. @[%s:%d]\n", __FILE__, __LINE__);
+		#endif
+		return 0;
+	}
+
+	if(NULL == (mapped_items = PyMapping_Items(env_obj)))
+	{
+		_report_error(PyExc_TypeError, "ERR: failed on getting mapped item from \"env\". @[%s:%d]", __FILE__, __LINE__);
+		return -2;
+	}
+
+	if(NULL == (iterator = PyObject_GetIter(mapped_items)))
+	{
+		_report_error(PyExc_TypeError, "ERR: failed on getting iterator from \"env\". @[%s:%d]", __FILE__, __LINE__);
+		Py_DECREF(mapped_items);
+		return -3;
+	}
+
+	v_current_ptr = env_buffer;
+	v_count = 0;
+	v_remain = MAX_ENVIRONMENT_BUFFER_SIZE;
+	while( (v_remain > 0) && (v_count < (MAX_ENVIRONMENT_COUNT-1)) && (item = PyIter_Next(iterator)) ) {
+		PyObject *env_name_obj;
+		PyObject *env_value_obj;
+		char *env_name;
+		char *env_value;
+
+		if(NULL != (env_name_obj = PyTuple_GetItem(item, 0)))
+		{
+			if(NULL == (env_name = PyString_AsString(env_name_obj)))
+			{
+				_report_error(NULL, "ERR: cannot get environment name content from %d-th pair of \"env\". @[%s:%d]", v_count, __FILE__, __LINE__);
+				Py_DECREF(item);
+				Py_DECREF(iterator);
+				Py_DECREF(mapped_items);
+				return -5;
+			}
+		}
+		else
+		{
+			_report_error(NULL, "ERR: cannot get environment name object from %d-th pair of \"env\". @[%s:%d]\n", v_count, __FILE__, __LINE__);
+			Py_DECREF(item);
+			Py_DECREF(iterator);
+			Py_DECREF(mapped_items);
+			return -4;
+		}
+
+		env_value = NULL;
+		if(NULL != (env_value_obj = PyTuple_GetItem(item, 1)))
+		{
+			if(NULL == (env_value = PyString_AsString(env_value_obj)))
+			{
+				_report_error(NULL, "ERR: cannot get environment value content from %d-th pair of \"env\". @[%s:%d]\n", v_count, __FILE__, __LINE__);
+				Py_DECREF(item);
+				Py_DECREF(iterator);
+				Py_DECREF(mapped_items);
+				return -6;
+			}
+		}
+
+		{
+			int l_name;
+			int l_value;
+			int l_total;
+
+			if(NULL == env_value)
+			{
+				l_name = strlen(env_name);
+				l_value = 0;
+				l_total = + 1;
+			}
+			else
+			{
+				l_name = strlen(env_name);
+				l_value = strlen(env_value);
+				l_total = l_name + l_value + 2;
+			}
+
+			if(v_remain < l_total)
+			{
+				_report_error(PyExc_MemoryError, "ERR: insufficient memory for loading \"env\". @[%s:%d]", __FILE__, __LINE__);
+				Py_DECREF(item);
+				Py_DECREF(iterator);
+				Py_DECREF(mapped_items);
+				return -7;
+			}
+
+			if(NULL == env_value)
+			{
+				strncpy(v_current_ptr, env_name, l_name);
+				v_current_ptr[l_name] = '\0';
+			}
+			else
+			{
+				strncpy(v_current_ptr, env_name, l_name);
+				v_current_ptr[l_name] = '=';
+				strncpy(v_current_ptr+l_name+1, env_value, l_value);
+				v_current_ptr[l_total-1] = '\0';
+			}
+
+			env_p[v_count] = v_current_ptr;
+
+			l_total = (0 != (l_total & CHILDPROCESS_BUFFER_ALLIGN_MASK)) ? (
+					l_total + (CHILDPROCESS_BUFFER_ALLIGN - (l_total & CHILDPROCESS_BUFFER_ALLIGN_MASK))
+				) : l_total;
+
+			v_current_ptr += l_total;
+			v_remain -= l_total;
+		}
+
+		v_count++;
+		Py_DECREF(item);
+	}
+
+	env_p[v_count] = NULL;
+
+	Py_DECREF(iterator);
+	Py_DECREF(mapped_items);
+
+	return 0;
+}
+
+
+static PyObject * ChildProcess_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+	coderunner_ChildProcessObject *self;
+
+	#if DUMP_DEBUG_MSG
+		fprintf(stderr, "INFO: new ChildProcess object. @[%s:%d]\n", __FILE__, __LINE__);
+	#endif
+
+	if( NULL == (self = (coderunner_ChildProcessObject *)(type->tp_alloc(type, 0))) )
+	{ return NULL; }
+
+	memset(&(self->childprocess_instance), 0, sizeof(CodeRunInstance));
+
+	return (PyObject *)(self);
+}
+
+static void ChildProcess_dealloc(coderunner_ChildProcessObject *self)
+{
+	#if DUMP_DEBUG_MSG
+		fprintf(stderr, "INFO: dealloc ChildProcess object. @[%s:%d]\n", __FILE__, __LINE__);
+	#endif
+
+	self->ob_type->tp_free( (PyObject *)(self) );
+}
+
+static int ChildProcess_init(coderunner_ChildProcessObject *self, PyObject *args, PyObject *kwds)
+{
+	static char *kwdlist[] = {"args", "env", "executable", "cwd", "run_as", "stdin", "stdout", "stderr", "max_run_second", "overtime_period_sigint_second", "overtime_period_sigterm_second", NULL};
+	static char *arglist = "O|OzzzzzzIII";
+
+	char arg_buffer[MAX_ARGUMENT_BUFFER_SIZE];
+	char *arg_v[MAX_ARGUMENT_BUFFER_SIZE];
+	char env_buffer[MAX_ENVIRONMENT_BUFFER_SIZE];
+	char *env_p[MAX_ENVIRONMENT_COUNT];
+
+	PyObject *arg_obj;
+	PyObject *env_obj;
+	char *filepath_executable;
+	char *working_directory;
+	char *runner_account;
+	char *filepath_stdin;
+	char *filepath_stdout;
+	char *filepath_stderr;
+	int max_runtime;
+	int ot_period_sigint;
+	int ot_period_sigterm;
+
+	int retcode;
+
+	arg_obj = NULL;
+	env_obj = NULL;
+	filepath_executable = NULL;
+	working_directory = NULL;
+	runner_account = NULL;
+	filepath_stdin = NULL;
+	filepath_stdout = NULL;
+	filepath_stderr = NULL;
+	max_runtime = 0;
+	ot_period_sigint = 0;
+	ot_period_sigterm = 0;
+
+	#if DUMP_DEBUG_MSG
+		fprintf(stderr, "INFO: init ChildProcess object. @[%s:%d]\n", __FILE__, __LINE__);
+	#endif
+
+	if( !PyArg_ParseTupleAndKeywords(args, kwds, arglist, kwdlist, &arg_obj, &env_obj, &filepath_executable, &working_directory, &runner_account, &filepath_stdin, &filepath_stdout, &filepath_stderr, &max_runtime, &ot_period_sigint, &ot_period_sigterm) )
+	{ return -1; }
+
+	if(0 != _ChildProcess_prepare_arg(arg_buffer, arg_v, arg_obj))
+	{ return -1; }
+
+	if(0 != _ChildProcess_prepare_env(env_buffer, env_p, env_obj))
+	{ return -1; }
+
+	if(NULL == filepath_executable)
+	{ filepath_executable = arg_v[0]; }
+
+	if(NULL == working_directory)
+	{ working_directory = "."; }
+
+
+	retcode = run_program(&(self->childprocess_instance),
+		filepath_executable,
+		arg_v, env_p,
+		working_directory,
+		runner_account,
+		filepath_stdin, filepath_stdout, filepath_stderr,
+		(uint32_t)(max_runtime),
+		(uint32_t)(ot_period_sigint),
+		(uint32_t)(ot_period_sigterm));
+
+	if(0 == retcode)
+	{ return 0; }
+
+	_report_error(PyExc_RuntimeError, "ERR: failed on activate run_program function (ret=%d). @[%s:%d]", retcode, __FILE__, __LINE__);
+	return -1;
+}
+
+
+static PyObject * _ChildProcess_wait_impl(coderunner_ChildProcessObject *self, int is_blocking)
+{
+	int retcode;
+
+	retcode = wait_program(&(self->childprocess_instance), is_blocking);
+
+	if(0 == retcode)
+	{ Py_RETURN_TRUE; }
+	else if(1 == retcode)
+	{ Py_RETURN_FALSE; }
+
+	_report_error(PyExc_RuntimeError, "ERR: failed on waiting program (errno=%d). @[%s:%d]", self->childprocess_instance.last_errno, __FILE__, __LINE__);
+	return NULL;
+}
+
+static PyObject * ChildProcess_wait_blocking(coderunner_ChildProcessObject *self)
+{
+	return _ChildProcess_wait_impl(self, 1);
+}
+
+static PyObject * ChildProcess_wait_nonblocking(coderunner_ChildProcessObject *self)
+{
+	return _ChildProcess_wait_impl(self, 0);
+}
+
+
+
+/*
+vim: ts=4 sw=4 ai nowrap
+*/
---
+++ python/ext/pycoderunner.c
@@ -0,0 +1,157 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdint.h>
+
+#include "coderunner.h"
+
+#define DUMP_DEBUG_MSG 1
+
+#ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
+	#define PyMODINIT_FUNC void
+#endif
+
+
+#define ____CODE_INDICATOR_05a04832113c7891f7e5dffd4ef5a9a396a25058 1
+
+
+PyMODINIT_FUNC initpycoderunner(void);
+
+
+static void _report_error(PyObject *errtype, const char *format, ...)
+{
+	char message_buf[1024];
+	va_list arg;
+
+	va_start(arg, format);
+	PyOS_vsnprintf(message_buf, 1024, format, arg);
+	va_end(arg);
+
+	#if DUMP_DEBUG_MSG
+		fputs(message_buf, stderr);
+	#endif	/* DUMP_DEBUG_MSG */
+
+	if(NULL != errtype)
+	{
+		PyErr_SetString(errtype, message_buf);
+	}
+}
+
+/* {{{ object data structure */
+typedef struct {
+	PyObject_HEAD
+
+	/* Type-specific fields */
+	CodeRunInstance childprocess_instance;
+} coderunner_ChildProcessObject;
+/* }}} object data structure */
+
+
+#include "childprocess.c"
+
+
+/** method table for ChildProcess class */
+static PyMethodDef coderunner_ChildProcess_methods[] = {
+	{"wait_blocking", (PyCFunction)ChildProcess_wait_blocking, METH_NOARGS,
+		"Blocking wait for program stop. Return True on program stopped." },
+	{"wait_nonblocking", (PyCFunction)ChildProcess_wait_nonblocking, METH_NOARGS,
+		"Blocking wait for program stop. Return True on program stopped, False if program still running." },
+	{NULL}  /* Sentinel */
+};
+
+static PyTypeObject coderunner_ChildProcessType = {
+	PyObject_HEAD_INIT(NULL)
+	0,							/* ob_size; */
+	"coderunner.ChildProcess",	/* const char	*tp_name; # For printing, in format "<module>.<name>" */
+
+	/* For allocation */
+	sizeof(coderunner_ChildProcessObject),	/* Py_ssize_t tp_basicsize; */
+	0,							/* Py_ssize_t	tp_itemsize; */
+
+	/* Methods to implement standard operations */
+	(destructor)(ChildProcess_dealloc),	/* destructor	tp_dealloc; */
+	0,							/* printfunc	tp_print; */
+	0,							/* getattrfunc	tp_getattr; */
+	0,							/* setattrfunc	tp_setattr; */
+	0,							/* cmpfunc		tp_compare; */
+	0,							/* reprfunc		tp_repr; */
+
+	/* Method suites for standard classes */
+	0,							/* PyNumberMethods		*tp_as_number; */
+	0,							/* PySequenceMethods	*tp_as_sequence */
+	0,							/* PyMappingMethods		*tp_as_mapping */
+
+	/* More standard operations (here for binary compatibility) */
+	0,							/* hashfunc		tp_hash; */
+	0,							/* ternaryfunc	tp_call; */
+	0,							/* reprfunc		tp_str; */
+	0,							/* getattrofunc	tp_getattro; */
+	0,							/* setattrofunc	tp_setattro; */
+
+	/* Functions to access object as input/output buffer */
+	0,							/* PyBufferProcs	*tp_as_buffer; */
+
+	( Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE ),	/* long	tp_flags; # Flags to define presence of optional/expanded features */
+	"Child process class for managing process status",	/* const char *tp_doc;	# Documentation string */
+
+	0,							/* traverseproc tp_traverse; # call function for all accessible objects */
+	0,							/* inquiry tp_clear; # delete references to contained objects */
+
+	0,							/* richcmpfunc tp_richcompare; # rich comparisons */
+	0,							/* Py_ssize_t tp_weaklistoffset; # weak reference enabler */
+
+	/* Iterators */
+	0,							/* getiterfunc tp_iter; */
+	0,							/* iternextfunc tp_iternext; */
+
+	/* Attribute descriptor and subclassing stuff */
+	coderunner_ChildProcess_methods,	/* struct PyMethodDef *tp_methods; */
+	0,							/* struct PyMemberDef *tp_members; */
+	0,							/* struct PyGetSetDef *tp_getset; */
+	0,							/* struct _typeobject *tp_base; */
+	0,							/* PyObject *tp_dict; */
+	0,							/* descrgetfunc tp_descr_get; */
+	0,							/* descrsetfunc tp_descr_set; */
+	0,							/* Py_ssize_t tp_dictoffset; */
+	(initproc)(ChildProcess_init),				/* initproc tp_init; */
+	0,							/* allocfunc tp_alloc; */
+	ChildProcess_new,				/* newfunc tp_new; */
+	0,							/* freefunc tp_free; # Low-level free-memory routine */
+	0,							/* inquiry tp_is_gc; # For PyObject_IS_GC */
+	0,							/* PyObject *tp_bases; */
+	0,							/* PyObject *tp_mro; # method resolution order */
+	0,							/* PyObject *tp_cache; */
+	0,							/* PyObject *tp_subclasses; */
+	0,							/* PyObject *tp_weaklist; */
+	0,							/* destructor tp_del; */
+#if 0	/* *** */
+	0,							/* unsigned int tp_version_tag; # Type attribute cache version tag. Added in version 2.6 */
+#endif
+};
+
+
+
+
+/** function table for module-level functions */
+static PyMethodDef coderunner_functions[] = {
+	{NULL}  /* Sentinel */
+};
+
+
+PyMODINIT_FUNC initbasemessage(void)
+{
+	PyObject* m;
+
+	if(PyType_Ready(&coderunner_ChildProcessType) < 0)
+	{ return; }
+
+	m = Py_InitModule3("coderunner", coderunner_functions, "Code runner module.");
+
+	Py_INCREF(&coderunner_ChildProcessType);
+	PyModule_AddObject(m, "ChildProcess", (PyObject *)&coderunner_ChildProcessType);
+}
+
+
+/*
+vim: ts=4 sw=4 ai nowrap
+*/
