14b58ed7c83a21fb094785386c6f80f49fe6b103
Yinyin L <yinyinl@gmail.com>
2013-08-19 01:35:37 +0800

    remove unused setup code

--- setup.py
+++ setup.py
@@ -4,13 +4,8 @@ import os
 import platform
 import re
 
-#from distutils import dep_util
-#from distutils import log
 from distutils.core import setup
 from distutils.core import Extension
-#from distutils.cmd import Command
-#from distutils.command.build_scripts import build_scripts as _build_scripts
-#from distutils.command.install_scripts import install_scripts as _install_scripts
 
 
 
@@ -23,8 +18,6 @@ extcoderunner_include = ['include', "python/ext/object_impl",]
 setup(name='childprocess',
 		version='1.0',
 		description='Class for creating and waiting for child processes complete',
-		packages=['coderunner',],
-		package_dir={'': 'lib'},
 		ext_modules=[
 			Extension("coderunner", extcoderunner_src, include_dirs=extcoderunner_include)
 		]
